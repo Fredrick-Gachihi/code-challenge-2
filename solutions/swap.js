d8bb1b4c3ad1e3604f0eb9dd86061589c99f60c7
fredrick-gachihi <fredrickgachihi24@gmail.com>
2024-04-01 11:16:23 +0300

    my second code challenge

--- solutions/swap.js
+++ solutions/swap.js
@@ -1,10 +1,10 @@
 //how to change upperCase to lowwerCase and viceverser
-let sentence = 'the quick brown fox';// since const can neither  be redeclared nor reasigned
+var sentence = 'the quick brown fox';// since const can neither  be redeclared nor reasigned
 
-let capitaliseString = (word)=> word[0].toLowerCase() + 
+var capitaliseString = (word)=> word[0].toLowerCase() + 
 word.slice(1).toUpperCase(); //the first word should be small while the rest big 
 
-let capitaliseWords =(word) =>word.split(' ').map
+var capitaliseWords =(word) =>word.split(' ').map
 (capitaliseString).join(' ');//to return an array as a string 
 
 console.log(capitaliseWords(sentence));
